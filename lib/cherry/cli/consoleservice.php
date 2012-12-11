1bb7754b9fdcd44177b67ad6618b742c13fcc4c5
Christopher Vagnetoft <cvagnetoft@gmail.com>
2012-12-11 20:15:19 +0100

    Signal- and exception handling in consoleservice fixed

--- lib/cherry/cli/consoleservice.php
+++ lib/cherry/cli/consoleservice.php
@@ -11,12 +11,29 @@ abstract class ConsoleService extends ConsoleApplication {
         $pid = pcntl_fork();
         if ($pid == 0) {
             // If we are the child
+            echo posix_getpid(),"\n";
             $this->serviceMain();
             exit;
+        } elseif($pid == -1) {
+            return false;
         } else {
+            return true;
             // Parent
         }
     }
+    
+    protected function attachSignal($signal, $handler, $restart=false) {
+        if (!is_callable($handler))
+            user_error("Signal handler is not callable.");
+        if ($signal === null) $signal = SIG_DFL;
+        if ($signal === false) $signal = SIG_IGN;
+        pcntl_signal($signal,$handler,$restart);
+    }
 
-
+    protected function log($str,$vararg=null) {
+         $args = func_get_args();
+         $lstr = call_user_func_array('sprintf',$args)."\n";
+         echo $lstr;
+    }
+    
 }
