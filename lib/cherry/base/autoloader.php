ec0ad78511917a7022abf539ead5dc1a5b13c3e8
Christopher Vagnetoft <cvagnetoft@gmail.com>
2012-12-13 20:31:42 +0100

    Updated autoloader

--- lib/cherry/base/autoloader.php
+++ lib/cherry/base/autoloader.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace Cherry\Autoloader;
-
+namespace Cherry\Autoloader {
+    
 class Autoloaders {
 
     private static $loaders = array();
@@ -86,3 +86,63 @@ class Autoloader {
 }
 
 class AutoloaderException extends \Exception { }
+
+}
+
+namespace Cherry\Base {
+
+    if (!defined('_NS_')) define('_NS_',"\\");
+
+    class AutoLoader {
+        private
+            $path = null,
+            $ns = null,
+            $options = [
+                'extensions' => '.php|.class.php'
+            ];
+        public function __construct($path,$ns=null,array $options=null) {
+            $this->path = $path;
+            if ($ns) {
+                $ns = trim($ns,_NS_)._NS_;
+                $this->ns = $ns;
+            }
+            $this->options = array_merge($this->options,(array)$options);
+        }
+        public function register() {
+            spl_autoload_register([&$this,'autoload'],true);
+        }
+        public function unregister() {
+            spl_autoload_unregister([&$this,'autoload'],true);
+        }
+        public function autoload($class) {
+            if ($this->ns) {
+                $cm = strtolower($class);
+                $nm = strtolower($this->ns);
+                if (substr($cm,0,strlen($nm)) == $nm) {
+                    $cf = substr($class,strlen($nm));
+                } else return false;
+            } else {
+                $cf = $class;
+            }
+            if (strpos($cf,'_')!==false) {
+                $cfn = str_replace('_',_DS_,$cf);
+            } else {
+                $cfn = str_replace("\\",'/',$cf);
+            }
+            $loc = $this->path._DS_;
+            $extn = (array)explode("|",$this->options['extensions']);
+            for($case = 0; $case < 2; $case++) {
+                foreach($extn as $ext) {
+                    $fl = $loc.(($case==1)?strtolower($cfn):$cfn).$ext;
+                    \Cherry\Debug("Autoload: Checking {$fl}");
+                    if (file_exists($fl) && is_readable($fl)) {
+                        require_once $fl;
+                        return true;
+                    }
+                }
+            }
+
+        }
+    }
+
+}
