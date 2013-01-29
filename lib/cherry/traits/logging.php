60c42e5bf74da9101cc296222659c56064f31204
Christopher Vagnetoft <cvagnetoft@gmail.com>
2013-01-29 15:51:11 +0100

    Logging trait for internal redirectable logging.

---
+++ lib/cherry/traits/logging.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Cherry\Traits;
+
+trait Logging {
+
+    private $_logcb = null;
+
+    protected function log($str) {
+        if (func_num_args()>1)
+            $str = call_user_func_array("sprintf",func_get_args());
+        if (!$this->_logcb)
+            echo $str."\n";
+        else
+            call_user_func($this->_logcb,$str);
+    }
+
+    public function setLogCallback(callable $cb = null) {
+        $this->_logcb = $cb;
+    }
+    public function getLogCallback() {
+        return $this->_logcb;
+    }
+
+
+}
