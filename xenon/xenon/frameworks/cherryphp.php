d70189fd181b291b584354b27e987499980b2b4a
Christopher Vagnetoft <cvagnetoft@gmail.com>
2013-02-15 22:56:26 +0100

    Xenon bootstrapping now takes an optional app root to define

--- xenon/xenon/frameworks/cherryphp.php
+++ xenon/xenon/frameworks/cherryphp.php
@@ -8,7 +8,7 @@ class cherryphp {
     public function addAutoloader($path,$base=null) {
         (new \Cherry\Base\Autoloader($path,$base))->register();
     }
-    public static function bootstrap() {
+    public static function bootstrap($approot=null) {
         static $done = 0;
         if ($done) return;
         $done = 1;
@@ -19,10 +19,14 @@ class cherryphp {
         if ($dm == 0) setenv("DEBUG=0");
         if ($dm >= 1) setenv("DEBUG=1");
         if ($dm >= 2) setenv("DEBUG_VERBOSE=1");
-        
         $path = getenv("CHERRY_LIB");
         require_once $path . '/lib/bootstrap.php';
-        \Cherry\Base\PathResolver::getInstance()->setAppPath(XENON_ROOT);
+        if (!$approot) {
+            $approot = getcwd();
+        } else {
+            $approot = realpath($approot);
+        }
+        \Cherry\Base\PathResolver::getInstance()->setAppPath($approot);
         
         define("XENON_FWAPI",'\xenon\frameworks\cherryphp');
     }
